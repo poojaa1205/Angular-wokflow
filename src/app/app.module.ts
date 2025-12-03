e65388495060fe8b7ebf17cfa74ff137cbcfc012
Pooja84601 <poojapatel2099@gmail.com>
2025-12-03 06:15:11 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { App } from './app';
+@NgModule({
+  declarations: [
+    App
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [App]
+})
+export class AppModule { }
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class App {
-  protected readonly title = signal('my-angular-demo');
+export class
+
+ App {
+  title = 'my-angular-demo';
 }
